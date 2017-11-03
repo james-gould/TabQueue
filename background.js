8aa54126337238f96260475e6db8516b322e4a0c
James <James@DESKTOP-UOH2H4A>
2017-11-03 22:53:44 +0000

    Storing tab data finished

--- background.js
+++ background.js
@@ -8,7 +8,28 @@ const contextProps = {
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+  ParseTab(tab);
+
+  /**
+   * Used if we fuck shit up by accident and need to clear storage, just uncomment and it'll wipe sync storage.
+   */
   
+  // chrome.storage.sync.clear(_ => {
+  // })
 });
+
+function ParseTab(tab) {
+  chrome.storage.sync.get((items) => {
+    Object.keys(items).length > 0 && items.data
+      ? items.data.push({ iconUrl: tab.favIconUrl, tabUrl: tab.url})
+      : items.data = [{ iconUrl: tab.favIconUrl, tabUrl: tab.url}]
+
+      chrome.storage.sync.set(items, _ => {
+        chrome.storage.sync.get((items) => {
+          alert(JSON.stringify(items));
+        })
+      })
+  });
+}
 
 chrome.contextMenus.create(contextProps);
