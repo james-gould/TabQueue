a25dbf188328acedde55680bb1ce9caada325f38
James <James@DESKTOP-UOH2H4A>
2017-11-03 21:46:39 +0000

    Context menu item created and working

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
 /**
  * Handles logic within the background script
  */
+
+const contextProps = { 
+    "title": "Queue this tab",
+    "enabled": true,
+    "onclick": HandleContextClick,
+}
+
+function HandleContextClick() {
+    alert('Finished!')
+}
+
+chrome.contextMenus.create(contextProps);
