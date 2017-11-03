4b87182541b3faf5fcb01a91504c93c60eae5558
James <James@DESKTOP-UOH2H4A>
2017-11-03 21:43:38 +0000

    icons created

--- popup.js
+++ popup.js
@@ -2,4 +2,14 @@
  * Handles logic contained within the popup window
  */
 
- 
+ const contextProps = { 
+     title: "Queue this tab",
+     enabled: true,
+     onclick: HandleContextClick,
+ }
+
+ function HandleContextClick() {
+
+ }
+
+ chrome.contextMenus.create(contextProps)
